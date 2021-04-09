61135c59cdd203c05a59b8dd9a92bd55c4f7ffd7
dhartley <dani_hartley@msn.com>
2021-04-09 13:50:33 -0500

    Combine arrays based user response

--- Develop/script.js
+++ Develop/script.js
@@ -12,55 +12,55 @@
 
 
 //Password Variables - Arrays
-var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var number = ['0','1','2','3','4','5','6','7','8','9'];
-var special =['!','@','#','$','%','^','&','*','(',')','+','=',',','.','<','>','?','/'];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '=', ',', '.', '<', '>', '?', '/'];
 
 // User prompts - Define password criteria
 
 // Generate Function
 
-var userInput = function() {
+var generatePassword = function () {
   // User chooses password length
-  var confirmlength = (prompt("How many characters would you like in your password? Choose between 8 and 128"));
-  while (confirmlength < 8 || confirmlength > 128) {
+  var confirmlength = parseInt(prompt("How many characters would you like in your password? Choose between 8 and 128"));
+  if (confirmlength < 8 || confirmlength > 128) {
     alert("Password length must be between 8-128 characters. Try again.");
-    return userInput();
+    return generatePassword();
   }
-  // User prompt for Characters
-  var includeLower =confirm("Do you want to include lowercase characters?");
-  var includeUpper =confirm("Do you want to include uppercase characters?");
-  var includeNumber =confirm("Do you want to include numeric characters?");
-  var includeSpecial =confirm("Do you want to include special characters?");
-  // Condition - must include characters
-  while (!includeLower && !includeUpper && !includeNumber && !includeSpecial) {
+  // User prompt for Characters 
+  // Need to figure out how to make it loop. 
+  var includeLower = confirm("Do you want to include lowercase characters?");
+  var includeUpper = confirm("Do you want to include uppercase characters?");
+  var includeNumber = confirm("Do you want to include numeric characters?");
+  var includeSpecial = confirm("Do you want to include special characters?");
+  // Condition - must include at least one characters type
+  if (!includeLower && !includeUpper && !includeNumber && !includeSpecial) {
     alert("Your password must contain at least one lowercase, uppercase, numeric, or special character");
-    return;
+    return generatePassword();
   }
-}
 
-userInput ();
+  // Combine all the arrays into a new array that are the user selected characters
+  var passwordCharacters = []
 
+  if (includeLower) {
+    passwordCharacters = passwordCharacters.concat(lowercase)
+  }
 
+  if (includeUpper) {
+    passwordCharacters = passwordCharacters.concat(uppercase)
+  }
 
+  if (includeNumber) {
+    passwordCharacters = passwordCharacters.concat(number)
 
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  if (includeSpecial) {
+    passwordCharacters = passwordCharacters.concat(special)
+  }
+  
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
